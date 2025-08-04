6704e2e3af5be054985a039eb2650d7e34f0209e
Md. Rafiul Islam <rislamshaon95@gmail.com>
2025-08-04 20:54:54 +0600

    topbar design is finished

--- src/common/Header/TopBar.jsx
+++ src/common/Header/TopBar.jsx
@@ -5,8 +5,8 @@ import { FaXTwitter } from "react-icons/fa6";
 const TopBar = () => {
     return (
         <div className="bg-[#ff084a] py-3 text-white">
-            <div className="container mx-auto">
-                <div className="flex gap-3">
+            <div className="container mx-auto flex items-center gap-3 justify-between">
+                <div className="flex items-center gap-3">
                     <a href="#" className="hover:text-gray-200">
                         <FaFacebookF className="h-5 w-5"/>
                     </a>
@@ -15,6 +15,14 @@ const TopBar = () => {
                     </a>
                     <a href="#" className="hover:text-gray-200">
                         <FaXTwitter className="h-5 w-5"/>
+                    </a>
+                </div>
+                <div className="text-center text-sm font-semibold">
+                    <p>We deliver products countrywide - Fast and reliable!</p>
+                </div>
+                <div className="text-sm text-center font-bold">
+                    <a href="tel:+8801980114434" className="hover:text-gray-200">
+                        +8801980-114434
                     </a>
                 </div>
             </div>
