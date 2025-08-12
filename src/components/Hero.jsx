b837754661b3a4c2a5a813b00fb22daf995b686c
Md. Rafiul Islam <rislamshaon95@gmail.com>
2025-08-12 20:21:17 +0600

    designed hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import bannerImg from '../../src/assets/banner.png';
+
+const Hero = () => {
+    return (
+        <div className="min-h-[400px] bg-gradient-to-br from-violet-700 to-alpha">
+            <div className="container mx-auto md:px-4 flex justify-between items-center md:gap-4 max-md:flex-col-reverse">
+                <div className="w-full pb-5 md:text-left text-center md:w-1/2 space-y-4">
+                    <h2 className='text-white lg:text-5xl md:text-4xl text-3xl font-bold'>Style That Speaks for You!</h2>
+                    <p className='text-white text-xl font-semibold'>Discover the latest trends and timeless <br /> classics—crafted for comfort, designed <br /> for confidence.</p>
+                    <button className='bg-alpha text-white hover:bg-beta px-6 py-4 rounded-xl font-medium transition-all shadow-xl'>Get Started</button>
+                </div>
+                <div className="w-full md:w-1/2 md:my-[100px] my-[50px] flex justify-center items-center">
+                <img src={bannerImg} className='w-[400px] h-[400px] bg-beta rounded-full' alt='banner image' />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
