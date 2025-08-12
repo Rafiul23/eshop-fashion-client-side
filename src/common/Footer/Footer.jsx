3bddfbee76e65ac9146c3a7d190daaf11cc387b0
Md. Rafiul Islam <rislamshaon95@gmail.com>
2025-08-12 17:36:20 +0600

    footer design completed

--- src/common/Footer/Footer.jsx
+++ src/common/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router";
 import { FaFacebookF } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
-
+import { FaPhoneAlt } from "react-icons/fa";
 
 const Footer = () => {
     return (
@@ -27,16 +27,16 @@ const Footer = () => {
                 <div>
                     <h3 className="text-lg text-gray-800 mb-4">Shop</h3>
                     <ul className="space-y-2 text-gray-600">
-                        <li className="hover:text-gray-600 transition-colors">
+                        <li className="hover:text-alpha transition-colors">
                             <Link to='/'>Men's Top wear</Link>
                         </li>
-                        <li className="hover:text-gray-600 transition-colors">
+                        <li className="hover:text-alpha transition-colors">
                             <Link to='/'>Women's Top wear</Link>
                         </li>
-                        <li className="hover:text-gray-600 transition-colors">
+                        <li className="hover:text-alpha transition-colors">
                             <Link to='/'>Men's Bottom wear</Link>
                         </li>
-                        <li className="hover:text-gray-600 transition-colors">
+                        <li className="hover:text-alpha transition-colors">
                             <Link to='/'>Women's Bottom wear</Link>
                         </li>
                     </ul>
@@ -46,16 +46,16 @@ const Footer = () => {
                  <div>
                     <h3 className="text-lg text-gray-800 mb-4">Support</h3>
                     <ul className="space-y-2 text-gray-600">
-                        <li className="hover:text-gray-600 transition-colors">
+                        <li className="hover:text-alpha transition-colors">
                             <Link to='/'>Contact Us</Link>
                         </li>
-                        <li className="hover:text-gray-600 transition-colors">
+                        <li className="hover:text-alpha transition-colors">
                             <Link to='/'>About Us</Link>
                         </li>
-                        <li className="hover:text-gray-600 transition-colors">
+                        <li className="hover:text-alpha transition-colors">
                             <Link to='/'>FAQs</Link>
                         </li>
-                        <li className="hover:text-gray-600 transition-colors">
+                        <li className="hover:text-alpha transition-colors">
                             <Link to='/'>Featurs</Link>
                         </li>
                     </ul>
@@ -65,18 +65,28 @@ const Footer = () => {
                 <div>
                     <h3 className="text-lg text-gray-800 mb-4">Follow Us</h3>
                     <div className="flex items-center gap-4 mb-4">
-                        <a href="https://www.facebook.com" target="_blank" className="hover:text-gray-500">
+                        <a href="https://www.facebook.com" target="_blank" className="hover:text-alpha">
                         <FaFacebookF />
                         </a>
-                        <a href="https://www.instagram.com" target="_blank" className="hover:text-gray-500">
+                        <a href="https://www.instagram.com" target="_blank" className="hover:text-alpha">
                         <FaInstagram />
                         </a>
-                        <a href="https://www.x.com" target="_blank" className="hover:text-gray-500">
+                        <a href="https://www.x.com" target="_blank" className="hover:text-alpha">
                         <FaXTwitter />
                         </a>
                     </div>
+
+                    <p className="text-gray-500">Call Us</p>
+                    <a href="tel:+8801980114434" className="mt-1">
+                        <FaPhoneAlt className="inline-block mr-2"/>
+                        +8801980-114434
+                    </a>
                 </div>
 
+            </div>
+
+            <div className="container mx-auto my-12 px-4 border-t border-gray-200 pt-6">
+                <p className="text-gray-600 text-center">@ 2025, All Rights Reserved.</p>
             </div>
         </footer>
     );
