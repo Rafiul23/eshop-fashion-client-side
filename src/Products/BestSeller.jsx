1b201573702c8bf0bb56d40c143e1eeb3d165271
Md. Rafiul Islam <rislamshaon95@gmail.com>
2025-08-15 17:41:22 +0600

    added a little functionality to add to cart button

--- src/Products/BestSeller.jsx
+++ src/Products/BestSeller.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { BsCart } from "react-icons/bs";
+import Swal from "sweetalert2";
 
 const selectedProduct = {
   name: "Stylish Jacket",
@@ -23,27 +24,51 @@ const selectedProduct = {
 };
 
 const BestSeller = () => {
+  const [mainImage, setMainImage] = useState("");
+  const [selectedSize, setSelectedSize] = useState("");
+  const [selectedColor, setSelectedColor] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
 
-    const [mainImage, setMainImage] = useState('');
-    const [selectedSize, setSelectedSize] = useState('');
-    const [selectedColor, setSelectedColor] = useState('');
-    const [quantity, setQuantity] = useState(1);
-    const [isButtonDisabled, setIsButtonDisabled] = useState(false);
-
-    useEffect(()=> {
-        if(selectedProduct?.images?.length > 0){
-            setMainImage(selectedProduct.images[0].url);
-        }
-    }, [])
-
-    const handleQuantity = (action) => {
-      if(action === "plus" ){
-        setQuantity(quantity + 1);
-      } 
-      if(action === "minus" && quantity > 1){
-        setQuantity(quantity - 1);
-      }
+  useEffect(() => {
+    if (selectedProduct?.images?.length > 0) {
+      setMainImage(selectedProduct.images[0].url);
     }
+  }, []);
+
+  const handleQuantity = (action) => {
+    if (action === "plus") {
+      setQuantity(quantity + 1);
+    }
+    if (action === "minus" && quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
+
+  const handleAddToCart = () => {
+    if (!selectedColor || !selectedSize) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!",
+        footer:
+          '<a href="#">Please, select the size and color before adding to cart.</a>',
+      });
+      return;
+    }
+    setIsButtonDisabled(true);
+
+    setTimeout(() => {
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Your product is added to cart successfully",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      setIsButtonDisabled(false);
+    }, 500);
+  };
 
   return (
     <div className="py-20">
@@ -62,7 +87,9 @@ const BestSeller = () => {
               {selectedProduct.images.map((image, index) => (
                 <img
                   key={index}
-                  className={`w-20 h-20 object-cover rounded-lg cursor-pointer border ${mainImage === image.url ? "border-black" : "border-gray-400"}`}
+                  className={`w-20 h-20 object-cover rounded-lg cursor-pointer border ${
+                    mainImage === image.url ? "border-black" : "border-gray-400"
+                  }`}
                   src={image.url}
                   alt={image.altText}
                   onClick={() => setMainImage(image.url)}
@@ -86,10 +113,12 @@ const BestSeller = () => {
               {selectedProduct.images.map((image, index) => (
                 <img
                   key={index}
-                  className={`w-20 h-20 object-cover rounded-lg cursor-pointer border ${mainImage === image.url ? "border-black" : "border-gray-400"}`}
+                  className={`w-20 h-20 object-cover rounded-lg cursor-pointer border ${
+                    mainImage === image.url ? "border-black" : "border-gray-400"
+                  }`}
                   src={image.url}
                   alt={image.altText}
-                  onClick={()=> setMainImage(image.url)}
+                  onClick={() => setMainImage(image.url)}
                 />
               ))}
             </div>
@@ -116,8 +145,12 @@ const BestSeller = () => {
                   {selectedProduct.colors.map((color) => (
                     <button
                       key={color}
-                      onClick={()=> setSelectedColor(color)}
-                      className={`w-8 h-8 rounded-full border ${selectedColor === color ? "border-4 border-alpha" : "border-gray-400"}`}
+                      onClick={() => setSelectedColor(color)}
+                      className={`w-8 h-8 rounded-full border ${
+                        selectedColor === color
+                          ? "border-4 border-alpha"
+                          : "border-gray-400"
+                      }`}
                       style={{
                         backgroundColor: color.toLocaleLowerCase(),
                         filter: "brightness(0.5)",
@@ -131,7 +164,15 @@ const BestSeller = () => {
                 <p className="text-lg text-gray-700">Sizes:</p>
                 <div className="flex gap-2 mt-2">
                   {selectedProduct.sizes.map((size) => (
-                    <button onClick={()=> setSelectedSize(size)} key={size} className={`px-4 py-2 rounded border ${selectedSize === size ? "bg-alpha text-white border-4 border-black" : ""}`}>
+                    <button
+                      onClick={() => setSelectedSize(size)}
+                      key={size}
+                      className={`px-4 py-2 rounded border ${
+                        selectedSize === size
+                          ? "bg-alpha text-white border-4 border-black"
+                          : ""
+                      }`}
+                    >
                       {size}
                     </button>
                   ))}
@@ -141,28 +182,44 @@ const BestSeller = () => {
               <div className="mb-4">
                 <p className="text-gray-700 text-xl">Quantity:</p>
                 <div className="flex items-center gap-4 mt-2">
-                  <button onClick={()=> handleQuantity("plus")} className="px-2 bg-gray-200 rounded text-lg cursor-pointer">+</button>
+                  <button
+                    onClick={() => handleQuantity("plus")}
+                    className="px-2 bg-gray-200 rounded text-lg cursor-pointer"
+                  >
+                    +
+                  </button>
                   <span className="text-lg">{quantity}</span>
-                  <button onClick={()=> handleQuantity("minus")} className="px-2 bg-gray-200 rounded text-lg cursor-pointer">-</button>
+                  <button
+                    onClick={() => handleQuantity("minus")}
+                    className="px-2 bg-gray-200 rounded text-lg cursor-pointer"
+                  >
+                    -
+                  </button>
                 </div>
               </div>
 
-              <button className="bg-alpha hover:bg-beta text-white px-4 py-2 rounded-lg w-full">Add to Cart <BsCart className="inline-block ml-2"/></button>
+              <button
+                onClick={handleAddToCart}
+                disabled={isButtonDisabled}
+                className={`bg-alpha hover:bg-beta text-white px-4 py-2 rounded-lg w-full ${isButtonDisabled ? "cursor-not-allowed opacity-50" : "hover:bg-beta"}`}
+              >
+                {isButtonDisabled ? "Adding ..." : "Add to Cart"}
+              </button>
 
               <div className="mt-10 text-gray-700">
-                  <h3 className="text-xl font-bold mb-4">Characteristics:</h3>
-                  <table className="w-full text-sm text-gray-600">
-                    <tbody>
-                        <tr className="border">
-                            <td className="py-1 pl-1">Brand:</td>
-                            <td className="py-1">{selectedProduct.brand}</td>
-                        </tr>
-                        <tr className="border">
-                            <td className="py-1 pl-1">Material:</td>
-                            <td className="py-1">{selectedProduct.material}</td>
-                        </tr>
-                    </tbody>
-                  </table>
+                <h3 className="text-xl font-bold mb-4">Characteristics:</h3>
+                <table className="w-full text-sm text-gray-600">
+                  <tbody>
+                    <tr className="border">
+                      <td className="py-1 pl-1">Brand:</td>
+                      <td className="py-1">{selectedProduct.brand}</td>
+                    </tr>
+                    <tr className="border">
+                      <td className="py-1 pl-1">Material:</td>
+                      <td className="py-1">{selectedProduct.material}</td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
