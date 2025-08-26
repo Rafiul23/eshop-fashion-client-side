278eb997739770a2cdc63d75efa0d1ba32980473
Md. Rafiul Islam <rislamshaon95@gmail.com>
2025-08-26 20:48:07 +0600

    designing login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import { useState } from "react";
 
 const Login = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
-        <div>
-            
+        <div className="flex">
+            <div className="w-full md:w-1/2 flex-col justify-center items-center p-8 md:p-12">
+                <form className="w-full max-w-md bg-white p-8 rounded-lg border shadow-lg">
+                    <div className="flex justify-center mb-6">
+                        <h2 className="text-xl font-semibold text-alpha">E-Shop Fashion</h2>
+                    </div>
+                    <h2 className="text-2xl font-bold text-center mb-6">Hello there!</h2>
+                    <p className="text-center mb-6">Enter Your Username and Password to login</p>
+                </form>
+            </div>
         </div>
     );
 };
