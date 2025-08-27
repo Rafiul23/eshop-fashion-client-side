b90332e35785b70539b5a1807798e307885413ee
Md. Rafiul Islam <rislamshaon95@gmail.com>
2025-08-27 19:42:29 +0600

    added eye button to hide and show password

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,38 +1,76 @@
-import { useState } from "react";
-import login from '../../assets/login.png';
+import login from "../../assets/login.png";
 import { Link } from "react-router";
+import { IoMdEye } from "react-icons/io";
+import { IoMdEyeOff } from "react-icons/io";
+import { useState } from "react";
 
 const Login = () => {
+  const [isHidden, setIsHidden] = useState(true);
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
 
-    return (
-        <div className="flex md:flex-row flex-col-reverse">
-            <div className="w-full md:w-1/2 flex flex-col md:flex-row justify-center items-center p-8 md:p-10">
-                <form className="w-full max-w-md bg-white p-8 rounded-lg border shadow-lg">
-                    <div className="flex justify-center mb-6">
-                        <h2 className="text-xl font-semibold text-alpha">E-Shop Fashion</h2>
-                    </div>
-                    <h2 className="text-2xl font-bold text-center mb-6">Hello there!</h2>
-                    <p className="text-center mb-6">Enter Your Email and Password to Login</p>
-                    <div className="mb-4">
-                        <label className="block text-sm font-semibold mb-2">Email</label>
-                        <input type="email" value={email} onChange={(e)=> setEmail(e.target.value)} required className="w-full p-2 border rounded" placeholder="Enter Your Email" />
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-sm font-semibold mb-2">Password</label>
-                        <input type="password" value={password} onChange={(e)=> setPassword(e.target.value)} required className="w-full p-2 border rounded" placeholder="Enter Your Password" />
-                    </div>
-                    <button className="w-full p-2 text-white bg-alpha hover:bg-beta rounded-xl">Login</button>
-                    <p className="mt-6 text-center">New on E-Shop Fashion? Please! <Link className="text-alpha underline font-bold" to='/signup'>Sign Up</Link></p>
-                </form>
-            </div>
-            <div className="w-full md:w-1/2 flex justify-center items-center py-4">
-                    <img src={login} alt="login" className="w-full h-full" />
-                </div>
+    console.log(email, password);
+  };
+
+  return (
+    <div className="flex md:flex-row flex-col-reverse">
+      <div className="w-full md:w-1/2 flex flex-col md:flex-row justify-center items-center p-8 md:p-10 relative">
+        <form
+          onSubmit={handleLogin}
+          className="w-full max-w-md bg-white p-8 rounded-lg border shadow-lg"
+        >
+          <div className="flex justify-center mb-6">
+            <h2 className="text-xl font-semibold text-alpha">E-Shop Fashion</h2>
+          </div>
+          <h2 className="text-2xl font-bold text-center mb-6">Hello there!</h2>
+          <p className="text-center mb-6">
+            Enter Your Email and Password to Login
+          </p>
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-2">Email</label>
+            <input
+              type="email"
+              name="email"
+              required
+              className="w-full p-2 border rounded"
+              placeholder="Enter Your Email"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-2">Password</label>
+            <input
+              type={isHidden ? "password" : "text"}
+              name="password"
+              required
+              className="w-full p-2 border rounded"
+              placeholder="Enter Your Password"
+            />
+          </div>
+          <button className="w-full p-2 text-white bg-alpha hover:bg-beta rounded-xl">
+            Login
+          </button>
+          <p className="mt-6 text-center">
+            New on E-Shop Fashion? Please!{" "}
+            <Link className="text-alpha underline font-bold" to="/signup">
+              Sign Up
+            </Link>
+          </p>
+        </form>
+        <div className="absolute bottom-[255px] right-[165px]">
+          <button onClick={() => setIsHidden(!isHidden)}>
+            {isHidden ? <IoMdEye /> : <IoMdEyeOff />}
+          </button>
         </div>
-    );
+      </div>
+      <div className="w-full md:w-1/2 flex justify-center items-center py-4">
+        <img src={login} alt="login" className="w-full h-full" />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,43 +1,87 @@
-import { useState } from "react";
 import { Link } from "react-router";
-import signup from '../../assets/signup.png';
+import signup from "../../assets/signup.png";
+import { IoMdEye } from "react-icons/io";
+import { IoMdEyeOff } from "react-icons/io";
+import { useState } from "react";
 
 const SignUp = () => {
 
-    const [email, setEmail] = useState('');
-        const [password, setPassword] = useState('');
-    
+    const [isHidden, setIsHidden] = useState(true);
+  
+  const handleSignUp = e =>{
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
 
-    return (
-        <div className="flex md:flex-row-reverse flex-col-reverse">
-            <div className="w-full md:w-1/2 flex flex-col md:flex-row justify-center items-center p-8 md:p-10">
-                <form className="w-full max-w-md bg-white p-8 rounded-lg border shadow-lg">
-                    <div className="flex justify-center mb-6">
-                        <h2 className="text-xl font-semibold text-alpha">E-Shop Fashion</h2>
-                    </div>
-                    <h2 className="text-2xl font-bold text-center mb-6">Hello there!</h2>
-                    <p className="text-center mb-6">Enter Your Name, Email and <br /> Password to Sign Up</p>
-                    <div className="mb-4">
-                        <label className="block text-sm font-semibold mb-2">Name</label>
-                        <input type="text" value={email} onChange={(e)=> setEmail(e.target.value)} required className="w-full p-2 border rounded" placeholder="Enter Your Name" />
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-sm font-semibold mb-2">Email</label>
-                        <input type="email" value={email} onChange={(e)=> setEmail(e.target.value)} required className="w-full p-2 border rounded" placeholder="Enter Your Email" />
-                    </div>
-                    <div className="mb-4">
-                        <label className="block text-sm font-semibold mb-2">Password</label>
-                        <input type="password" value={password} onChange={(e)=> setPassword(e.target.value)} required className="w-full p-2 border rounded" placeholder="Enter Your Password" />
-                    </div>
-                    <button className="w-full p-2 text-white bg-alpha hover:bg-beta rounded-xl">Sign Up</button>
-                    <p className="mt-6 text-center">Already have an account? Please! <Link className="text-alpha underline font-bold" to='/login'>Login</Link></p>
-                </form>
-            </div>
-            <div className="w-full md:w-1/2 flex justify-center items-center py-4">
-                    <img src={signup} alt="signup" className="w-full h-full" />
-                </div>
+    console.log(name, email, password);
+  }
+
+  return (
+    <div className="flex md:flex-row-reverse flex-col-reverse">
+      <div className="w-full md:w-1/2 flex flex-col md:flex-row justify-center items-center p-8 md:p-10 relative">
+        <form onSubmit={handleSignUp} className="w-full max-w-md bg-white p-8 rounded-lg border shadow-lg">
+          <div className="flex justify-center mb-6">
+            <h2 className="text-xl font-semibold text-alpha">E-Shop Fashion</h2>
+          </div>
+          <h2 className="text-2xl font-bold text-center mb-6">Hello there!</h2>
+          <p className="text-center mb-6">
+            Enter Your Name, Email and <br /> Password to Sign Up
+          </p>
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-2">Name</label>
+            <input
+              type="text"
+              name='name'
+              required
+              className="w-full p-2 border rounded"
+              placeholder="Enter Your Name"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-2">Email</label>
+            <input
+              type="email"
+              name="email"
+              required
+              className="w-full p-2 border rounded"
+              placeholder="Enter Your Email"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-2">Password</label>
+            <input
+              type={ isHidden ? "password" : "text"}
+              name="password"
+              required
+              className="w-full p-2 border rounded"
+              placeholder="Enter Your Password"
+            />
+          </div>
+          <button className="w-full p-2 text-white bg-alpha hover:bg-beta rounded-xl">
+            Sign Up
+          </button>
+          <p className="mt-6 text-center">
+            Already have an account? Please!{" "}
+            <Link className="text-alpha underline font-bold" to="/login">
+              Login
+            </Link>
+          </p>
+        </form>
+        <div className="absolute bottom-[182px] right-[165px]">
+            <button onClick={()=> setIsHidden(!isHidden)}>
+                {
+                    isHidden ? <IoMdEye /> : <IoMdEyeOff />
+                }
+            </button>
         </div>
-    );
+      </div>
+      <div className="w-full md:w-1/2 flex justify-center items-center py-4">
+        <img src={signup} alt="signup" className="w-full h-full" />
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
