9be39efef503f7fce2a806de74a2d607aad96643
Md. Rafiul Islam <rislamshaon95@gmail.com>
2026-06-14 17:41:34 +0600

    written all options for filtered side bar

--- src/Products/FilterSideBar.jsx
+++ src/Products/FilterSideBar.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+import { useSearchParams } from "react-router";
 
 const FilterSideBar = () => {
+    const [searchParams, setSearchParams] = useSearchParams('');
+    const [filters, setFilters] = useState({
+        category: "",
+        color: "",
+        gender: "",
+        size: [],
+        material: [],
+        brand: [],
+        minPrice: 0,
+        maxPrice: 100
+    });
+
+    const [priceRange, setPriceRange] = useState([0, 100]);
+    const categories = ["Top Wear", "Bottom Wear"];
+    const colors = ["Red", "Blue", "Green", "Yellow", "Pink", "White", "Black", "Gray", "Beige", "Navy"];
+    const sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
+    const materials = ['Wool', 'Cotton', 'Denim', 'Polyestar', 'Silk', 'Linen', 'Viscose', 'Flecce'];
+    const brands = ['Urban Threads', 'Modern Fit', 'Street Style', 'Beach Breeze', 'Fashionista', 'ChicStyle'];
+    const genders = ['Men', 'Women'];
+    
     return (
         <div>
             
