11f9bdf125d96a357122593b8df1e7287a37f041
Md. Rafiul Islam <rislamshaon95@gmail.com>
2026-06-14 15:51:57 +0600

    built sidebar logic

--- src/pages/CollectionPage/CollectionPage.jsx
+++ src/pages/CollectionPage/CollectionPage.jsx
@@ -1,17 +1,48 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { FaFilter } from "react-icons/fa";
+import FilterSideBar from "../../Products/FilterSideBar";
 
 const CollectionPage = () => {
   const [products, setProducts] = useState([]);
+  const sideBarRef = useRef(null);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const handleClickOutside = (e) => {
+    if (sideBarRef.current && !sideBarRef.current.contains(e.target)) {
+      setIsSidebarOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    // add eventlistener
+    document.addEventListener("mousedown", handleClickOutside);
+    // remove eventlistener
+    document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
 
   useEffect(() => {
     fetch("/public/products.json")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
-  
-  return <div>
 
-  </div>;
+  return (
+    <div className="flex flex-col lg:flex-row">
+      {/* mobile filter button */}
+      <button onClick={toggleSidebar} className="lg:hidden border p-2 flex justify-center items-center">
+        <FaFilter className="mr-2"></FaFilter>
+      </button>
+
+      {/* filter sidebar */}
+      <div ref={sideBarRef} className={`${isSidebarOpen ? "translate-x-0" : "translate-x-full"}`}>
+        <FilterSideBar></FilterSideBar>
+      </div>
+    </div>
+  );
 };
 
 export default CollectionPage;
