848733c1cf6f72d7e88b1673557df0d90cb4f3ce
Md. Rafiul Islam <rislamshaon95@gmail.com>
2026-06-15 16:03:24 +0600

    design for gender is done

--- src/Products/FilterSideBar.jsx
+++ src/Products/FilterSideBar.jsx
@@ -45,6 +45,7 @@ const FilterSideBar = () => {
     return (
         <div className="p-4">
             <h3 className="text-xl font-semibold text-gray-800 mb-4">Filter</h3>
+
             {/* category filter */}
             <div className="mb-6">
                 <label className="text-gray-600 font-medium mb-2 block">Category</label>
@@ -57,6 +58,20 @@ const FilterSideBar = () => {
                     ))
                 }
             </div>
+
+            {/* gender filter */}
+            <div className="mb-6">
+                <label className="text-gray-600 font-medium mb-2 block">Gender</label>
+                {
+                    genders.map((gender)=> (
+                        <div key={gender} className="flex items-center mb-1">
+                            <input type="radio" name="category" className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border-gray-300" id="" />
+                            <span className="text-gray-700">{gender}</span>
+                        </div>
+                    ))
+                }
+            </div>
+
         </div>
     );
 };
