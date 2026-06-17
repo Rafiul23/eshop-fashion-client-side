4e71f0fcbfe11ca3ed60f8b95dcf2345b62f41d9
Md. Rafiul Islam <rislamshaon95@gmail.com>
2026-06-17 20:37:19 +0600

    developping checkout page and collction for shipping address

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
 
 const Checkout = () => {
-    return (
-        <div>
-            test
-        </div>
-    );
+  const cart = {
+    products: [
+      {
+        _id: 1,
+        name: "Stylish Jacket",
+        size: "M",
+        color: "Black",
+        price: 120,
+        image: "https://picsum.photos/200?random=1",
+      },
+      {
+        _id: 2,
+        name: "Stylish Jacket",
+        size: "L",
+        color: "Red",
+        price: 120,
+        image: "https://picsum.photos/200?random=2",
+      }
+    ],
+    totalPrice: 240
+  };
+
+  const navigate = useNavigate();
+  const [shippingAddress, setShippingAddress] = useState({
+    firstName: "",
+    lastName: "",
+    address: "",
+    city: "",
+    postalCode: "",
+    country: "",
+    phone: ""
+  });
+
+  return <div></div>;
 };
 
 export default Checkout;
