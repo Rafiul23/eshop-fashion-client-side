c90d941beb7c2a202e717f649f895b90bb5e4dcd
Md. Rafiul Islam <rislamshaon95@gmail.com>
2026-06-20 15:48:27 +0600

    designing the left section of checkout page

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -35,7 +35,15 @@ const Checkout = () => {
     phone: ""
   });
 
-  return <div></div>;
+  return <div className='grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-7xl mx-auto py-10 px-6 tracking-tighter'>
+    {/* left section */}
+    <div className='bg-white p-6 rounded-lg'>
+      <h2 className='text-2xl uppercase mb-6'>Checkout</h2>
+      <form>
+        <h3 className=''></h3>
+      </form>
+    </div>
+  </div>;
 };
 
 export default Checkout;
