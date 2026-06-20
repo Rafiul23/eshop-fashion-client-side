397f1d2900b8f5074174f58216a2a2ae8f3fe054
Md. Rafiul Islam <rislamshaon95@gmail.com>
2026-06-20 16:18:36 +0600

    designing checkout form

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -40,7 +40,31 @@ const Checkout = () => {
     <div className='bg-white p-6 rounded-lg'>
       <h2 className='text-2xl uppercase mb-6'>Checkout</h2>
       <form>
-        <h3 className=''></h3>
+        <h3 className='text-lg mb-4'>Cntact Details</h3>
+        <div className='mb-4'>
+        <label className='block text-gray-700 mb-2'>Email</label>
+        <input type="email" name="" value={"user@example.com"} className='w-full p-2 border rounded' id="" />
+        </div>
+        <div className=''>
+          <h3 className='text-lg mb-4'>Delivary</h3>
+          <div className='mb-4 grid grid-cols-2 gap-4'>
+            <div>
+              <label className='block text-gray-700 mb-2'>First Name:</label>
+              <input type="text" onChange={(e)=> setShippingAddress({...shippingAddress, firstName: e.target.value})} name="" value={"Alex"} className='w-full p-2 border rounded' required id="" />
+            </div>
+            <div>
+              <label className='block text-gray-700 mb-2'>Last Name:</label>
+              <input type="text" onChange={(e)=> setShippingAddress({...shippingAddress, lastName: e.target.value})} name="" value={"Gooth"} className='w-full p-2 border rounded' required id="" />
+            </div>
+          </div>
+            <div className='mb-4'>
+              <label className='block text-gray-700 mb-2'>Address:</label>
+              <input type="text" name="" onChange={(e)=> setShippingAddress({...shippingAddress, address: e.target.value})} value={shippingAddress.address} className='w-full p-2 border rounded' required id="" />
+            </div>
+            <div className='mb-4 grid grid-cols-2 gap-4'>
+      
+            </div>
+        </div>
       </form>
     </div>
   </div>;
